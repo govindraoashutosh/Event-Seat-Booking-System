8904ded4916377eeaedc63b8911e647e7d2d65d9
Ashutosh Govind Rao <govindraoashutosh@gmail.com>
2026-09-11 17:29:33 +0530

    added js code

---
+++ script.js
@@ -0,0 +1,38 @@
+const seats = [];
+
+for(i = 1; i<= 60; i++){
+    let category;
+    let price;
+    if(i <= 20){
+        category = "VIP";
+        price = 500;
+
+    }
+    else if(i <= 40){
+        category = "premium";
+        price = 300;
+    }
+    else {
+        category = "Regular";
+        price = 150;
+    }
+
+    seats.push({
+    id : i,
+    price : price,
+    category : category,
+    status : "available"
+
+})
+}
+
+ const seatContainer = document.getElementById("seat-container");
+ seats.forEach(function(seat){
+     const seatElement = document.createElement("div");
+     seatElement.classList.add("seat");
+     seatElement.classList.add(seat.status);
+     seatElement.textContent = seat.id;
+
+     seatContainer.appendChild(seatElement);
+
+ });
