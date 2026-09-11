612442a54c813570889fe3de34f6780aa751ad87
Ashutosh Govind Rao <govindraoashutosh@gmail.com>
2026-09-11 22:37:58 +0530

    added event listener

--- script.js
+++ script.js
@@ -35,4 +35,21 @@ for(i = 1; i<= 60; i++){
 
      seatContainer.appendChild(seatElement);
 
+     seatElement.addEventListener("click", function(){
+        if(seat.status === "available"){
+            seat.status = "selected";
+            seatElement.classList.remove("available");
+            seatElement.classList.add("selected");
+        }
+
+        else if(seat.status === "selected"){
+            seat.status = "available";
+            seatElement.classList.remove("selected");
+            seatElement.classList.add("available");
+
+        }
+
+        
+     })
+
  });
